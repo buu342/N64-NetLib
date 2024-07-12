3dd42bbf499a8ee18a38d799b0eaca50ace4844c
Buu342 <buu342@hotmail.com>
2024-07-12 03:25:59 +0100

    Generalizing master server connection handler thread

--- Master Server/src/ClientConnectionThread.java
+++ Master Server/src/ClientConnectionThread.java
@@ -3,6 +3,7 @@ import java.util.Hashtable;
 import java.util.Enumeration;
 import java.io.DataOutputStream;
 import NetLib.N64Server;
+import java.io.IOException;
 
 public class ClientConnectionThread implements Runnable {
 
@@ -14,28 +15,32 @@ public class ClientConnectionThread implements Runnable {
         this.clientsocket = socket;
     }
     
+    private void ListServers() throws IOException
+    {
+        DataOutputStream dos = new DataOutputStream(this.clientsocket.getOutputStream());
+        Enumeration<String> keys = this.servers.keys();
+        System.out.println("Sending client "+this.clientsocket+" the list of servers");
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            N64Server server = this.servers.get(key);
+            byte[] serverbytes = server.toByteArray();
+            if (serverbytes != null)
+            {
+            	final String packetype = "SERVER";
+                dos.write("N64PKT".getBytes());
+                dos.writeInt(serverbytes.length + packetype.length());
+                dos.write(packetype.getBytes());
+                dos.write(serverbytes);
+                dos.flush();
+            }
+        }
+        dos.close();
+    }
+    
     public void run() {
         try {
-            DataOutputStream dos = new DataOutputStream(this.clientsocket.getOutputStream());
-            Enumeration<String> keys = this.servers.keys();
-            System.out.println("Sending client "+this.clientsocket+" the list of servers");
-            while (keys.hasMoreElements()) {
-                String key = keys.nextElement();
-                N64Server server = this.servers.get(key);
-                byte[] serverbytes = server.toByteArray();
-                if (serverbytes != null)
-                {
-                	final String packetype = "SERVER";
-	                dos.write("N64PKT".getBytes());
-	                dos.writeInt(serverbytes.length + packetype.length());
-	                dos.write(packetype.getBytes());
-	                dos.write(serverbytes);
-	                dos.flush();
-                }
-                Thread.sleep(1000);
-            }
+        	this.ListServers();
             System.out.println("Finished with "+this.clientsocket);
-            dos.close();
             this.clientsocket.close();
         } catch (Exception e) {
             System.err.println(e);
