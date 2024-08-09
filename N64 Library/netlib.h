342d917227e00a3f1a4cd432b995e2587bdc0ba2
Buu342 <buu342@hotmail.com>
2024-08-09 22:59:26 +0100

    Started basic library outline

--- N64 Library/netlib.h
+++ N64 Library/netlib.h
@@ -1,6 +1,44 @@
 #ifndef _N64_NETLIB_H
 #define _N64_NETLIB_H
     
+    void netlib_initialize();
     
+    
+    /*==============================================================
+                        N64 -> Network Functions
+    ==============================================================*/
+    
+    void netlib_start();
+    
+    void netlib_writebyte();
+    
+    void netlib_writeword();
+    
+    void netlib_writedword();
+    
+    void netlib_writebytes();
+    
+    void netlib_broadcast();
+    
+    void netlib_sendtoserver();
+    
+    void netlib_sendtoclient();
+    
+    
+    /*==============================================================
+                        Network -> N64 Functions
+    ==============================================================*/
+    
+    void netlib_register();
+    
+    void netlib_poll();
+    
+    void netlib_readbyte();
+    
+    void netlib_readword();
+    
+    void netlib_readdword();
+    
+    void netlib_readbytes();
     
 #endif
