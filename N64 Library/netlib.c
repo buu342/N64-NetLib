2e7f30f724f072fd6c8df2be463d6b9591f6a1ed
Buu342 <buu342@hotmail.com>
2024-08-15 03:21:54 +0100

    Started C side implementation

--- N64 Library/netlib.c
+++ N64 Library/netlib.c
@@ -1,3 +1,13 @@
 #include "netlib.h"
 #include "usb.h"
 
+static size_t global_writecursize = 0;
+static byte global_writebuffer[MAX_PACKETSIZE];
+
+static size_t global_curfuncptrs = 0;
+void (*global_funcptrs[MAX_UNIQUEPACKETS])(size_t, ClientNumber);
+
+void netlib_initialize()
+{
+    usb_initialize();
+}
