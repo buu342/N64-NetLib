0b77f6ea96c2573c112a59c949d82d6605f8fddc
Buu342 <buu342@hotmail.com>
2024-08-16 03:24:07 +0100

    netlib_start function

--- N64 Library/netlib.c
+++ N64 Library/netlib.c
@@ -1,8 +1,9 @@
 #include "netlib.h"
 #include "usb.h"
 
-static size_t global_writecursize = 0;
-static byte global_writebuffer[MAX_PACKETSIZE];
+static size_t    global_writecursize = 0;
+static NetPacket global_curpacketid = 0;
+static byte      global_writebuffer[MAX_PACKETSIZE];
 
 static size_t global_curfuncptrs = 0;
 void (*global_funcptrs[MAX_UNIQUEPACKETS])(size_t, ClientNumber);
@@ -10,4 +11,10 @@ void (*global_funcptrs[MAX_UNIQUEPACKETS])(size_t, ClientNumber);
 void netlib_initialize()
 {
     usb_initialize();
+}
+
+bool netlib_start(NetPacket id)
+{
+    global_curpacketid = id;
+    global_writecursize = 0;
 }
